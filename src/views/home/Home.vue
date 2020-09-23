fbe77164172974ec4d0b54a7d3e4af75c2910f1f
Think <is.chenlei@outlook.com>
2020-09-23 10:27:56 +0800

    导航栏和首页轮播图

---
+++ src/views/home/Home.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <swiper>
+      <swiper-item v-for="item in banners">
+        <a :href="item.link">
+          <img :src="item.image" alt="">
+        </a>
+      </swiper-item>
+    </swiper>
+  </div>
+</template>
+
+<script>
+import NavBar from "@/components/common/navbar/NavBar";
+import {getHomeMultidata} from "@/network/home";
+import Swiper from "@/components/common/swiper/Swiper";
+import SwiperItem from "@/components/common/swiper/SwiperItem";
+
+export default {
+  name: "home",
+  components: {
+    SwiperItem,
+    Swiper,
+    NavBar
+  },
+  data() {
+    return {
+      banners: [],
+      // dKeyword: [],
+      // keywords: [],
+      recommends: []
+    }
+  },
+  created() {
+    //1.请求多个数据
+    getHomeMultidata().then(res => {
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    })
+  }
+}
+</script>
+
+<style scoped>
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
+</style>
